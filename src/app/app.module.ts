64446d3826af5cf393730ec1c73a3ff1140a210b
Nick <nikitamm59rus@gmail.com>
2020-10-17 15:36:32 +0300

    Add task front ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { dbInteractionService } from 'src/services/db_service/dbInteractionService';
-import { FormsModule }   from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NotificationBarComponent } from './notification-bar/notification-bar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatBadgeModule} from '@angular/material/badge';
 import { AutocheckerComponent } from './autochecker/autochecker.component';
 import { TaskComponent } from './task/task.component';
 import { MainComponentComponent } from './main-component/main-component.component';
 
-
-
+// Angular material
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 
 
 @NgModule({
@@ -35,11 +41,21 @@ import { MainComponentComponent } from './main-component/main-component.componen
     dbInteractionService,
     FormsModule,
     BrowserAnimationsModule,
+
+    // Angular Material
     MatMenuModule,
     MatIconModule,
     MatButtonModule,
-    MatBadgeModule
-    
+    MatBadgeModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatAutocompleteModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatCardModule,
+    MatDividerModule,
+
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {environment} from '../../environments/environment';
-import {HttpClient} from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-task',
@@ -8,15 +11,28 @@ import {HttpClient} from '@angular/common/http';
   styleUrls: ['./task.component.css']
 })
 export class TaskComponent implements OnInit {
+  myControl = new FormControl();
+  options: string[] = ["Раз раз", "Два два", "Три три"]
+  filteredOptions: Observable<string[]>;
 
   constructor(private http: HttpClient) { }
   inputText = '';
   ngOnInit(): void {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value))
+    );
   }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.options.filter(option => option.toLowerCase().indexOf(filterValue) === 0);
+  }
+
   postSpellCheckerData(data: string): void {
     this.http.get(environment.spellCheckerAPI + '?text=' + data).subscribe(value => {
-        this.fixSpelling(value);
-      },
+      this.fixSpelling(value);
+    },
       error => {
         console.log(error);
       });
