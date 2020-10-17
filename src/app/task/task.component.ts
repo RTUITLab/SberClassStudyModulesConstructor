c820bd4531b07df65b236f7e2ed7ee85ebf12637
Nick <nikitamm59rus@gmail.com>
2020-10-18 02:03:46 +0300

    Styled task and main pages

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -57,14 +57,15 @@ export class TaskComponent implements OnInit {
     return this.options.filter(option => option.toLowerCase().indexOf(filterValue) === 0);
   }
 
-  postSpellCheckerData(data: any): void {
-    this.http.get(environment.spellCheckerAPI + '?text=' + data.value).subscribe(value => {
-      this.myForm.controls[data.name].setValue(this.fixSpelling(value, data.value));
-    },
-      error => {
-        console.log(error);
-      });
-  }
+  // postSpellCheckerData(data: any): void {
+  //   this.http.get(environment.spellCheckerAPI + '?text=' + data.value).subscribe(value => {
+  //     this.myForm.controls[data.name].setValue(this.fixSpelling(value, data.value));
+  //   },
+  //     error => {
+  //       console.log(error);
+  //     });
+  // }
+
   fixSpelling(value: any, data: string): string {
     value.forEach(elem => {
       data = data.replace(elem.word, elem.s[0] || elem.word);
@@ -72,8 +73,43 @@ export class TaskComponent implements OnInit {
     return data;
   }
 
-  onFocusOut(event: any): void {
-    this.postSpellCheckerData(event.target);
+  checkTask(): void {
+    let controls: string[] = [
+      "task_name",
+      "task_author",
+      "task_outsource",
+      "task_notes_checker",
+      "task_notes_teacher",
+      "task_notes_student",
+      "subject",
+      "description"
+    ];
+    for (let i = 0; i < controls.length; i++) {
+      const key = controls[i];
+      let old_val = this.myForm.controls[key].value;
+
+      this.http.get(environment.spellCheckerAPI + '?text=' + old_val).subscribe(value => {
+        let new_val = this.fixSpelling(value, old_val)
+        this.myForm.controls[key].setValue(new_val);
+        if (old_val !== new_val) {
+          let value: string = document.getElementById(key).getAttribute("class");
+          if (value.indexOf("changed") === -1) {
+            document.getElementById(key).setAttribute("class", value + " changed")
+          }
+        } else {
+          let value: string = document.getElementById(key).getAttribute("class");
+          if (value.indexOf("changed") !== -1) {
+            let left = value.slice(0, value.indexOf("changed"));
+            let right = value.slice(value.indexOf("changed") + "changed".length, value.length);
+            document.getElementById(key).setAttribute("class", left + right)
+          }
+        }
+      },
+        error => {
+          console.log(error);
+        });
+
+    }
   }
 
   createTask(): void {
