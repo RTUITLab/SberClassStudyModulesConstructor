68eaa93c8e6c3d20657e0e7628ef6e68d4e08711
Komar Bogdan <chompski@inbox.ru>
2020-10-17 12:28:30 +0300

    Added form spell check

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {environment} from '../../environments/environment';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-task',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private http: HttpClient) { }
+  inputText = '';
   ngOnInit(): void {
   }
-
+  postSpellCheckerData(data: string): void {
+    this.http.get(environment.spellCheckerAPI + '?text=' + data).subscribe(value => {
+        this.fixSpelling(value);
+      },
+      error => {
+        console.log(error);
+      });
+  }
+  fixSpelling(value: any): void {
+    value.forEach(elem => {
+      this.inputText = this.inputText.replace(elem.word, elem.s[0] || elem.word);
+    });
+    console.log(this.inputText);
+  }
+  onKey(event: any): void {
+    this.inputText = event.target.value;
+  }
+  onFocusOut(event: any): void {
+    this.postSpellCheckerData(this.inputText);
+  }
 }
